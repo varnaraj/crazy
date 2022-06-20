7eeb4ca6f81d167361e0fe0a014141138a514902
varnaraj <varnaraj28@gmail.com>
2022-06-21 00:06:22 +0530

    Update

--- 1 phone number/main.py
+++ 1 phone number/main.py
@@ -9,7 +9,7 @@ ch_number=phonenumbers.parse(number,"CH")
 #print(ch_number)
 print(geocoder.description_for_number(ch_number,"en"))
 
-#Find service provider
+#Find service provider d
 from phonenumbers import carrier
 service_number=phonenumbers.parse(number,"RO")
 print(carrier.name_for_number(service_number,"en"))
