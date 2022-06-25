026e3500b635f2f8b158d986fb8d4e105e0a9194
varnaraj <varnaraj28@gmail.com>
2022-06-25 21:32:10 +0530

    Update main.py

--- 4 internet speed check/main.py
+++ 4 internet speed check/main.py
@@ -1,6 +1,6 @@
-import speedtest
-wifi  = speedtest()
-print("Wifi Download Speed is ", wifi.download())
-print("Wifi Upload Speed is ", wifi.upload())
+# import speedtest
+# wifi  = speedtest()
+# print("Wifi Download Speed is ", wifi.download())
+# print("Wifi Upload Speed is ", wifi.upload())
 
-#couldn't work
+# #couldn't work
