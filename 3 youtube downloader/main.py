7e64d994fed652eaf3a280f6549959aa8eef9166
varnaraj <varnaraj28@gmail.com>
2022-06-28 23:32:44 +0530

    Update main.py

--- 3 youtube downloader/main.py
+++ 3 youtube downloader/main.py
@@ -1,8 +1,18 @@
 #import package
-
+from pytube import YouTube
+url="https://www.youtube.com/watch?v=_6nKjFloSDs&ab_channel=AdityaMusic"
+my_video=YouTube(url)
 print("Video title")
-
+print(my_video.title)
 print("Video thumpnail")
+print(my_video.thumbnail_url)
 
 print("Video download video")
+
+#set stream resolution
+my_video=my_video.streams.get_highest_resolution()
+my_video.download()
+
+#check its live or not
+print(my_video.is_live)
 
