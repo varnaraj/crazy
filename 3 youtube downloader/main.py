4ec6a4348a59a75ac18cbc86037dd8a50ce5e920
varnaraj <varnaraj28@gmail.com>
2022-06-29 23:02:05 +0530

    Update main.py

--- 3 youtube downloader/main.py
+++ 3 youtube downloader/main.py
@@ -1,5 +1,7 @@
 #import package
+import imp
 from pytube import YouTube
+
 url="https://www.youtube.com/watch?v=_6nKjFloSDs&ab_channel=AdityaMusic"
 my_video=YouTube(url)
 print("Video title")
@@ -10,9 +12,14 @@ print(my_video.thumbnail_url)
 print("Video download video")
 
 #set stream resolution
-my_video=my_video.streams.get_highest_resolution()
-my_video.download()
+my_video=my_video.streams.get_lowest_resolution()
+#my_video.download()
 
 #check its live or not
 print(my_video.is_live)
 
+from pytube import Playlist
+#download a play list
+playlist= Playlist("https://www.youtube.com/playlist?list=PLkTq2HJgz1Ap8WRsoXQhdDkfryxOBpdxv")
+for video in plagylist:
+    video.streams.get_lowest_resolution().download()
