1d96e77aa6abfde6c28fb73652e1cf1e7bb938d9
varnaraj <varnaraj28@gmail.com>
2022-06-29 23:22:09 +0530

    Update main.py

--- 3 youtube downloader/main.py
+++ 3 youtube downloader/main.py
@@ -18,8 +18,16 @@ my_video=my_video.streams.get_lowest_resolution()
 #check its live or not
 print(my_video.is_live)
 
-from pytube import Playlist
+
 #download a play list
+from pytube import Playlist
+
+i=0
 playlist= Playlist("https://www.youtube.com/playlist?list=PLkTq2HJgz1Ap8WRsoXQhdDkfryxOBpdxv")
-for video in plagylist:
-    video.streams.get_lowest_resolution().download()
+
+print('Number of videos in playlist: %s' % len(playlist.video_urls))
+for video in playlist.video_urls:
+    print(video)
+    my_video1=YouTube(video)
+    my_video1=my_video1.streams.get_lowest_resolution()
+    #my_video1.download()
